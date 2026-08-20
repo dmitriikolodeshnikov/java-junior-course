76a7eb44725cb70aff2f48dfcbf6c738fd9f32b5
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-08-20 21:54:12 +0300

    day 1: first java program

--- src/week_01_java_basics/day_01_first_program/src/Main.java
+++ src/week_01_java_basics/day_01_first_program/src/Main.java
@@ -1,4 +1,21 @@
 package week_01_java_basics.day_01_first_program.src;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println("\t\tJAVA DEVELOPER");
+        System.out.println("===================================");
+
+
+        System.out.println("Name: Dmitrii");
+        System.out.println("Age: 36");
+        System.out.println("City: Saint-Petersburg");
+        System.out.println("Position: Beginner Java Developer");
+        System.out.println("Language: Java");
+        System.out.println();
+        System.out.println("Goal:");
+        System.out.println("Become a Junior Java Backend Developer");
+        System.out.println();
+        System.out.println("===================================");
+    }
 }
