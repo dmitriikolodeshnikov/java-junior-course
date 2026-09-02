6fda583937fb788c2c3baed5e87f92107f9585f8
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2026-09-02 22:51:02 +0300

    day 19: encapsulation

--- src/week_03/day_19_encapsulation/src/BankAccount.java
+++ src/week_03/day_19_encapsulation/src/BankAccount.java
@@ -1,4 +1,37 @@
 package week_03.day_19_encapsulation.src;
 
 public class BankAccount {
+    private double balance;
+
+    BankAccount(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+    }
+
+    void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid amount");
+            return;
+        }
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid amount");
+            return;
+        }
+
+        if (amount > balance) {
+            System.out.println("Insufficient funds");
+            return;
+        }
+
+        balance -= amount;
+    }
+
+    void showBalance() {
+        System.out.println("Balance: " + balance);
+    }
 }
--- src/week_03/day_19_encapsulation/src/Main.java
+++ src/week_03/day_19_encapsulation/src/Main.java
@@ -1,4 +1,16 @@
 package week_03.day_19_encapsulation.src;
 
 public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000);
+
+        account.showBalance();
+
+        account.deposit(500);
+        account.showBalance();
+
+        account.withdraw(300);
+        account.withdraw(2000);
+        account.showBalance();
+    }
 }
